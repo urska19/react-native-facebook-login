9614ca68d43b6d5c6a5913be5bbb233472f3a857
Yusuke Shibata <shibata@fata.io>
2017-11-23 18:46:52 +0900

    fix for removed View.propTypes

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@ import React, {
 
 import {
   View,
+  ViewPropTypes,
   StyleSheet,
   NativeModules,
   NativeMethodsMixin,
@@ -89,7 +90,7 @@ class FBLogin extends Component {
 }
 
 FBLogin.propTypes = {
-  style: View.propTypes.style,
+  style: ViewPropTypes.style,
   permissions: PropTypes.array, // default: ["public_profile", "email"]
   loginBehavior: PropTypes.number, // default: Native
   onLogin: PropTypes.func,
